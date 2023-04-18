f73652c0bc540ab40564821f9bccb6490bb1dfca
Pravat-21 <pravatpatra1997@gmail.com>
2023-04-18 21:13:17 +0530

    web application

--- src/utils.py
+++ src/utils.py
@@ -39,3 +39,10 @@ def elavuate_model_report(X_train,y_train,X_test,y_test,models):
         logging.info("error has occured in the evaluate_model_report function")
         raise CustomException(e,sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
